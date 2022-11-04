bc8ec38703e91aabd8c852d64172d1c0cf3f487b
Laine Vili V P <vvplaine@cubbli22-gold.local>
2022-11-04 22:33:14 +0200

    gitignore added, base libnfc functions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
+#include <iostream>
 extern "C" {
   #include <nfc/nfc.h>
   #include <nfc/nfc-types.h>
+}
+
+int main() {
+  nfc_context *context;
+  nfc_connstring connstring = "pn532_uart:/dev/ttyUSB0";
+  nfc_init(&context);
+  //std::cout << nfc_open(context, connstring);
+  return 0;
 }
