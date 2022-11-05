7bc0ce94d234624fbe89b0d5ed5e203b15e7c71c
Kirs67 <kirilel200267@gmail.com>
2022-11-05 18:22:45 +0200

    Added dumpping

--- rdr/rdr.cpp
+++ rdr/rdr.cpp
@@ -8,6 +8,7 @@
 #include <iterator>
 #include <algorithm>
 #include <iostream>
+#include <fstream>
 #include <iomanip>
 #include <stdexcept>
 
@@ -22,6 +23,8 @@ extern "C" {
 	// declare func
 	int pn53x_transceive(struct nfc_device *pnd, const uint8_t *pbtTx, const size_t szTx, uint8_t *pbtRx, const size_t szRxLen, int timeout);
 }
+
+std::ofstream datafile;
 
 std::string bytes_to_string(std::vector<uint8_t> data, std::string prefix="", std::string separator="") {
     std::stringstream ss;
@@ -73,17 +76,123 @@ std::vector<uint8_t> try_starting(struct nfc_device *pnd) {
     return resp;
 }
 
-std::vector<uint8_t> select_AID(struct nfc_device *pnd, std::vector<uint8_t> AID) {
-    std::vector<uint8_t> select_cmd = {0x40, 0x01, 0x00, 0xA4, 0x04, 0x00};
+std::vector<uint8_t> select_AID(struct nfc_device *pnd, std::vector<uint8_t> AID, uint8_t p1=0x04, uint8_t p2=0x00) {
+    std::vector<uint8_t> select_cmd = {0x40, 0x01, 0x00, 0xA4, p1, p2};
     select_cmd.push_back((uint8_t)AID.size());
     select_cmd.insert( select_cmd.end(), AID.begin(), AID.end() );
     select_cmd.push_back(0x00);
+
+    std::cout << bytes_to_string(select_cmd) << std::endl;
+
     return pn53x_transceive(pnd, select_cmd);
 }
 
 std::vector<uint8_t> read_record(struct nfc_device *pnd, uint8_t p1, uint8_t p2) {
     std::vector<uint8_t> read_cmd = {0x40, 0x01, 0x00, 0xB2, p1, p2, 0x00};
     return pn53x_transceive(pnd, read_cmd);
+}
+
+void dump_TLV(std::vector<uint8_t> data) {
+    datafile << bytes_to_string(data);
+}
+
+void decode_TLV(std::vector<uint8_t> data) {
+    std::cout << "https://emvlab.org/tlvutils/?data=" << bytes_to_string(data, "", "") << std::endl;
+#if 0
+    /* Look for cardholder name */
+    uint8_t * res = data.data();
+    unsigned char output[50], c, amount[10],msg[100];
+    int szRx = data.size();
+    unsigned int i, j, expiry;
+    for (i=0;i<(unsigned int) szRx-1;i++) {
+            if (*res==0x5f&&*(res+1)==0x20) {
+                strncpy(output, res+3, (int) *(res+2));
+                output[(int) *(res+2)]=0;
+                printf("Cardholder name: %s\n",output);            
+            }
+            res++;
+    }
+
+    /* Look for PAN & Expiry date */
+    res = data.data();
+    for (i=0;i<(unsigned int) szRx-1;i++) {
+        if ((*res==0x4d&&*(res+1)==0x57)||(*res==0x9f&&*(res+1)==0x6b)) {
+            strncpy(output, res+3, 13);
+            output[11]=0;
+            std::cout << "PAN:";
+            
+            for (j=0;j<8;j++) {
+                if (j%2==0) std::cout << " ";
+                c = output[j];
+                if (MASKED&j>=1&j<=5) {
+                    std::cout << "**";
+                }
+                else {
+                    printf("%02x",c&0xff);
+                }
+            }
+            std::cout << std::endl;
+            expiry = (output[10]+(output[9]<<8)+(output[8]<<16))>>4;
+            printf("Expiration date: %02x/20%02x\n\n",(expiry&0xff),((expiry>>8)&0xff));
+            break;            
+        }
+        res++;
+    }       
+
+    /* Look for public certificates */
+    res = data.data();
+    szRx = data.size();
+    for (i=0;i<(unsigned int) szRx-1;i++) {
+        if (*res==0x9f && *(res+1)==0x46 && *(res+2)==0x81) {
+            printf("ICC Public Key Certificate:\n");
+            int k;
+            for (k=4;k<(int)148;k++) {
+                printf("%02x",(unsigned int)*(res+k));
+            }
+            printf("\n\n");
+            break;            
+        }
+        res++;
+    }
+    
+    /* Look for public certificates */
+    res = data.data();
+    szRx = data.size();
+    for (i=0;i<(unsigned int) szRx-1;i++) {
+        if (*res==0x90 && *(res+1)==0x81 && *(res+2)==0xb0) {
+            printf("Issuer Public Key Certificate:\n");
+            int k;
+            for (k=3;k<(int)173;k++) {
+                printf("%02x",(unsigned int)*(res+k));
+            }
+            printf("\n\n");
+            break;            
+        }
+        res++;
+    }     
+
+    // Looking for transaction logs
+    szRx = data.size();
+    if (szRx==18) { // Non-empty transaction
+        res = data.data();
+
+        /* Look for date */
+        sprintf(msg,"%02x/%02x/20%02x",res[14],res[13],res[12]);
+
+        /* Look for transaction type */
+        if (res[15]==0) {
+            sprintf(msg,"%s %s",msg,"Payment");
+        }
+        else if (res[15]==1) {
+            sprintf(msg,"%s %s",msg,"Withdrawal");
+        }
+        
+        /* Look for amount*/
+        sprintf(amount,"%02x%02x%02x",res[3],res[4],res[5]);
+        sprintf(msg,"%s\t%d,%02x€",msg,atoi(amount),res[6]);
+        printf("%s\n",msg);
+    }
+#endif
 }
 
 std::map<std::string, std::vector<uint8_t>> get_AIDs_from_PSE(std::vector<uint8_t> pse) {
@@ -182,8 +291,9 @@ std::map<std::string, std::vector<uint8_t>> get_AIDs(nfc_device* pnd) {
     if(resp.empty())
         resp = select_AID(pnd, PSE);
     if(!resp.empty() && resp[0] == 0x6F) {
-        std::vector<uint8_t> pse(resp.begin() + 2, resp.end());
+        std::vector<uint8_t> pse(resp.begin() + 2, resp.end() - 2);
         std::cout << "[+] Got PSE/PPSE:" << std::endl << bytes_to_string(pse) << std::endl;
+        dump_TLV(pse);
         AIDs = get_AIDs_from_PSE(pse);
     } else {
         AIDs = try_known_AIDs(pnd);
@@ -192,105 +302,6 @@ std::map<std::string, std::vector<uint8_t>> get_AIDs(nfc_device* pnd) {
     std::cout << "[+] Found " << AIDs.size() << " AIDs" << std::endl;
 
     return AIDs;
-}
-
-void decode_TLV(std::vector<uint8_t> data) {
-    std::cout << "https://emvlab.org/tlvutils/?data=" << bytes_to_string(data) << std::endl;
-#if 0
-    /* Look for cardholder name */
-    uint8_t * res = data.data();
-    unsigned char output[50], c, amount[10],msg[100];
-    int szRx = data.size();
-    unsigned int i, j, expiry;
-    for (i=0;i<(unsigned int) szRx-1;i++) {
-            if (*res==0x5f&&*(res+1)==0x20) {
-                strncpy(output, res+3, (int) *(res+2));
-                output[(int) *(res+2)]=0;
-                printf("Cardholder name: %s\n",output);            
-            }
-            res++;
-    }
-
-    /* Look for PAN & Expiry date */
-    res = data.data();
-    for (i=0;i<(unsigned int) szRx-1;i++) {
-        if ((*res==0x4d&&*(res+1)==0x57)||(*res==0x9f&&*(res+1)==0x6b)) {
-            strncpy(output, res+3, 13);
-            output[11]=0;
-            std::cout << "PAN:";
-            
-            for (j=0;j<8;j++) {
-                if (j%2==0) std::cout << " ";
-                c = output[j];
-                if (MASKED&j>=1&j<=5) {
-                    std::cout << "**";
-                }
-                else {
-                    printf("%02x",c&0xff);
-                }
-            }
-            std::cout << std::endl;
-            expiry = (output[10]+(output[9]<<8)+(output[8]<<16))>>4;
-            printf("Expiration date: %02x/20%02x\n\n",(expiry&0xff),((expiry>>8)&0xff));
-            break;            
-        }
-        res++;
-    }       
-
-    /* Look for public certificates */
-    res = data.data();
-    szRx = data.size();
-    for (i=0;i<(unsigned int) szRx-1;i++) {
-        if (*res==0x9f && *(res+1)==0x46 && *(res+2)==0x81) {
-            printf("ICC Public Key Certificate:\n");
-            int k;
-            for (k=4;k<(int)148;k++) {
-                printf("%02x",(unsigned int)*(res+k));
-            }
-            printf("\n\n");
-            break;            
-        }
-        res++;
-    }
-    
-    /* Look for public certificates */
-    res = data.data();
-    szRx = data.size();
-    for (i=0;i<(unsigned int) szRx-1;i++) {
-        if (*res==0x90 && *(res+1)==0x81 && *(res+2)==0xb0) {
-            printf("Issuer Public Key Certificate:\n");
-            int k;
-            for (k=3;k<(int)173;k++) {
-                printf("%02x",(unsigned int)*(res+k));
-            }
-            printf("\n\n");
-            break;            
-        }
-        res++;
-    }     
-
-    // Looking for transaction logs
-    szRx = data.size();
-    if (szRx==18) { // Non-empty transaction
-        res = data.data();
-
-        /* Look for date */
-        sprintf(msg,"%02x/%02x/20%02x",res[14],res[13],res[12]);
-
-        /* Look for transaction type */
-        if (res[15]==0) {
-            sprintf(msg,"%s %s",msg,"Payment");
-        }
-        else if (res[15]==1) {
-            sprintf(msg,"%s %s",msg,"Withdrawal");
-        }
-        
-        /* Look for amount*/
-        sprintf(amount,"%02x%02x%02x",res[3],res[4],res[5]);
-        sprintf(msg,"%s\t%d,%02x€",msg,atoi(amount),res[6]);
-        printf("%s\n",msg);
-    }
-#endif
 }
 
 void try_reading_sector(nfc_device* pnd, uint8_t p1, uint8_t p2) {
@@ -307,7 +318,10 @@ void try_reading_sector(nfc_device* pnd, uint8_t p1, uint8_t p2) {
         std::cout << std::endl << "> READ RECORD " << std::hex
             << std::setw(2) << std::setfill('0') << (unsigned int) p1 << "-" 
             << std::setw(2) << std::setfill('0') << (unsigned int) p2 << " suceeded" << std::endl;
-        decode_TLV(std::vector<uint8_t>( resp.begin(), resp.end() - 2 ) );
+
+        std::vector<uint8_t> tlv(resp.begin(), resp.end() - 2);
+        dump_TLV(tlv);
+        decode_TLV(tlv);
     }
 }
 
@@ -317,6 +331,8 @@ static void close(nfc_device*pnd, nfc_context* context) {
         nfc_close(pnd);
     if(context)
         nfc_exit(context);
+    if(datafile)
+        datafile.close();
 }
 
 int main(int argc, char **argv) {
@@ -324,40 +340,49 @@ int main(int argc, char **argv) {
     nfc_device* pnd;
     nfc_modulation nm;
     nfc_target ant[1];
-    
-    std::cout << "[-] Connecting to the reader..." << std::endl;
-    nfc_init(&context);
-    pnd = nfc_open(context,NULL);
-    if (pnd == NULL) {
-        std::cout << "[x] Unable to connect to NFC device." << std::endl;
-        close(pnd, context);
-        return(1);
-    }
 
-    std::cout << "[+] Connected to NFC reader" << std::endl;
-    nfc_initiator_init(pnd);
+    try {
+        std::cout << "[-] Connecting to the reader..." << std::endl;
+        nfc_init(&context);
+        pnd = nfc_open(context,NULL);
+        if (pnd == NULL) {
+            std::cout << "[x] Unable to connect to NFC device." << std::endl;
+            close(pnd, context);
+            return(1);
+        }
 
-    // Checking card type...
-    std::cout << "[-] Looking for known card types..." << std::endl;
-    
-    auto resp = try_starting(pnd);
-    if(!resp.empty()) {
+        std::cout << "[+] Connected to NFC reader" << std::endl;
+        nfc_initiator_init(pnd);
 
-        std::cout << "[-] Searching for AIDs..." << std::endl;
-
-        auto AIDs = get_AIDs(pnd);
+        // Checking card type...
+        std::cout << "[-] Looking for known card types..." << std::endl;
         
-        for(auto AID: AIDs) {
-            std::cout << "[-] Selecting app \"" << AID.first << "\"..." << std::endl; 
-            select_AID(pnd, AID.second);
-            // Looking for data in the records
-            for (int p1 = 0; p1 <= 10; p1 += 1) {
-                for (int p2 = 12; p2 <= 28; p2 += 8) {
-                    try_reading_sector(pnd, p1, p2);
+        auto resp = try_starting(pnd);
+        if(!resp.empty()) {
+            datafile.open("data.txt");
+
+            std::cout << "[-] Searching for AIDs..." << std::endl;
+
+            auto AIDs = get_AIDs(pnd);
+            
+            for(auto AID: AIDs) {
+                std::cout << "[-] Selecting app \"" << AID.first << "\"..." << std::endl;
+                resp = select_AID(pnd, AID.second);
+                if(resp[0] != 0x6F)
+                    continue;
+
+                // Looking for data in the records
+                for (int ef = 1; ef <= 31; ef++) {
+                    for (int rec = 0; rec <= 16; rec++) {
+                        try_reading_sector(pnd, rec, (ef << 3) | 4);
+                    }
                 }
             }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
+    } catch(std::runtime_error ex) {
+        close(pnd, context);
+        throw ex;
     }
 
     close(pnd, context);
