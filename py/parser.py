eec787f84dd426f13ebe80d785e7f89ccd13a147
Laine Vili V P <vvplaine@cubbli22-gold.local>
2022-11-05 19:08:49 +0200

    Python parser multiplatform support, better cleanup of TLVP output

--- py/parser.py
+++ py/parser.py
@@ -1,15 +1,19 @@
 import subprocess
 import re
+import platform
 
-forbidden = ["[0m] ", '', '[0m\\n']
-
-p = subprocess.run(["./py/tlvp", "./py/data.txt"], capture_output=True)
+if (platform.system() == 'Linux'):
+  p = subprocess.run(["./py/tlvp", "./py/data.txt"], capture_output=True)
+elif (platform.system() == 'Windows'):
+  p = subprocess.run(["./py/tlvp.exe", "./py/data.txt"], capture_output=True)
 a = str(p)[78:]
 b = a.split('\\x1b')
 c = []
 
 for text in b:
-  m = re.sub(r'\[\d{1,2}[m;](\d{2}m)?(\\n)?(\s*)((\\[x])(.{2}))*|(\\[x])(.{2})', '', str(text))
+  m = re.sub(r'\[\d{1,2}[m;](\d{2}m)?(\\n)?(\s*)((\\[x])(.{2}))*|(\\[x])(.{2})|\[|\]|(Size:.*\n)|^\s/gm', '', str(text))
+  m = re.sub(r'^\s*', '', m)
+  m = re.sub(r'(S[a-z]{3}:\s)(\d{2})(\sbyte(s)?)', '', m)
   c.append(m)
 
 while "   " in c:
